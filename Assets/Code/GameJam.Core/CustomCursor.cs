72e9d83b6a5067c651c0a02f720696ea8219cb85
Colin Bellino <bellino.colin@gmail.com>
2020-04-20 22:51:50 +0200

    ✨ Update exit

--- Assets/Code/GameJam.Core/CustomCursor.cs
+++ Assets/Code/GameJam.Core/CustomCursor.cs
@@ -5,13 +5,13 @@ namespace GameJam.Core
 {
 	public class CustomCursor : MonoBehaviour
 	{
-		[SerializeField] [Required] private Texture2D _cursorTexture;
-		private CursorMode _cursorMode = CursorMode.Auto;
-		private Vector2 _hotSpot = Vector2.zero;
-		private void Awake()
-		{
-			Cursor.SetCursor(_cursorTexture, _hotSpot, _cursorMode);
-		}
+		// [SerializeField] [Required] private Texture2D _cursorTexture;
+		// private CursorMode _cursorMode = CursorMode.Auto;
+		// private Vector2 _hotSpot = Vector2.zero;
+		// private void Awake()
+		// {
+		// 	Cursor.SetCursor(_cursorTexture, _hotSpot, _cursorMode);
+		// }
 	}
 
 }
