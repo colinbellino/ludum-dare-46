6e307b540c3f027919d7923ba910db474fc4102e
Colin Bellino <bellino.colin@gmail.com>
2020-04-21 00:41:24 +0200

    ✨ Add level 9 and update level resources

--- Assets/Code/GameJam.Core/Data/Level.cs
+++ Assets/Code/GameJam.Core/Data/Level.cs
@@ -7,9 +7,10 @@ namespace GameJam.Core
 	[CreateAssetMenu(fileName = "Level", menuName = "GameJam/Level")]
 	public class Level : SerializedScriptableObject
 	{
-		public Dictionary<Structure, int> Structures = new Dictionary<Structure, int>();
+		[Required]
+		public Dictionary<Structure, int> Structures = new Dictionary<Structure, int> { };
 
-		// Start : 	{ Structure = 0, Fire = 1 }
+		// Start : 	{ Structure = 0, Terrain = 99, Fire = 1 }
 		// Tree :	{ Structure = 0 }
 		// Rock : 	{ Structure = 9 }
 		// Exit : 	{ Structure = 99 }
@@ -20,8 +21,8 @@ namespace GameJam.Core
 			{ new Vector2Int(0, 1), new Cell { } },
 			{ new Vector2Int(0, 2), new Cell { } },
 			{ new Vector2Int(0, 3), new Cell { } },
-			{ new Vector2Int(0, 4), new Cell { Structure = 9 } },
-			{ new Vector2Int(0, 5), new Cell { Structure = 99 } },
+			{ new Vector2Int(0, 4), new Cell { } },
+			{ new Vector2Int(0, 5), new Cell { } },
 			{ new Vector2Int(0, 6), new Cell { } },
 			{ new Vector2Int(0, 7), new Cell { } },
 			{ new Vector2Int(0, 8), new Cell { } },
@@ -29,12 +30,12 @@ namespace GameJam.Core
 
 			{ new Vector2Int(1, 0), new Cell { } },
 			{ new Vector2Int(1, 1), new Cell { } },
-			{ new Vector2Int(1, 2), new Cell { Structure = 0, Fire = 1 } },
+			{ new Vector2Int(1, 2), new Cell { Structure = 0, Terrain = 99, Fire = 1 } },
 			{ new Vector2Int(1, 3), new Cell { } },
 			{ new Vector2Int(1, 4), new Cell { Structure = 9 } },
-			{ new Vector2Int(1, 5), new Cell { Structure = 0 } },
+			{ new Vector2Int(1, 5), new Cell { Structure = 9 } },
 			{ new Vector2Int(1, 6), new Cell { } },
-			{ new Vector2Int(1, 7), new Cell { } },
+			{ new Vector2Int(1, 7), new Cell { Structure = 99 } },
 			{ new Vector2Int(1, 8), new Cell { } },
 			{ new Vector2Int(1, 9), new Cell { } },
 
@@ -42,19 +43,19 @@ namespace GameJam.Core
 			{ new Vector2Int(2, 1), new Cell { } },
 			{ new Vector2Int(2, 2), new Cell { } },
 			{ new Vector2Int(2, 3), new Cell { } },
-			{ new Vector2Int(2, 4), new Cell { Structure = 9 } },
-			{ new Vector2Int(2, 5), new Cell { Structure = 0 } },
-			{ new Vector2Int(2, 6), new Cell { Structure = 0 } },
-			{ new Vector2Int(2, 7), new Cell { Structure = 0 } },
-			{ new Vector2Int(2, 8), new Cell { Structure = 0 } },
-			{ new Vector2Int(2, 9), new Cell { Structure = 99 } },
+			{ new Vector2Int(2, 4), new Cell { } },
+			{ new Vector2Int(2, 5), new Cell { } },
+			{ new Vector2Int(2, 6), new Cell { } },
+			{ new Vector2Int(2, 7), new Cell { } },
+			{ new Vector2Int(2, 8), new Cell { } },
+			{ new Vector2Int(2, 9), new Cell { } },
 
 			{ new Vector2Int(3, 0), new Cell { } },
 			{ new Vector2Int(3, 1), new Cell { } },
 			{ new Vector2Int(3, 2), new Cell { } },
 			{ new Vector2Int(3, 3), new Cell { } },
-			{ new Vector2Int(3, 4), new Cell { Structure = 9 } },
-			{ new Vector2Int(3, 5), new Cell { Structure = 0 } },
+			{ new Vector2Int(3, 4), new Cell { } },
+			{ new Vector2Int(3, 5), new Cell { } },
 			{ new Vector2Int(3, 6), new Cell { } },
 			{ new Vector2Int(3, 7), new Cell { } },
 			{ new Vector2Int(3, 8), new Cell { } },
@@ -62,12 +63,12 @@ namespace GameJam.Core
 
 			{ new Vector2Int(4, 0), new Cell { } },
 			{ new Vector2Int(4, 1), new Cell { } },
-			{ new Vector2Int(4, 2), new Cell { } },
+			{ new Vector2Int(4, 2), new Cell { Structure = 99 } },
 			{ new Vector2Int(4, 3), new Cell { } },
 			{ new Vector2Int(4, 4), new Cell { } },
-			{ new Vector2Int(4, 5), new Cell { Structure = 99 } },
+			{ new Vector2Int(4, 5), new Cell {  } },
 			{ new Vector2Int(4, 6), new Cell { } },
-			{ new Vector2Int(4, 7), new Cell { } },
+			{ new Vector2Int(4, 7), new Cell { Structure = 99 } },
 			{ new Vector2Int(4, 8), new Cell { } },
 			{ new Vector2Int(4, 9), new Cell { } },
 
