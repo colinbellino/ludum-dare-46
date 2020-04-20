774b7cd80481d16bb04bccd8630a60a1a69bc21e
Erehuc <huc.romain@gmail.com>
2020-04-20 21:43:05 +0200

    ✨ Add (fancy) cursor

---
+++ Assets/Code/GameJam.Core/CustomCursor.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameJam.Core
+{
+	public class CustomCursor : MonoBehaviour
+	{
+		[SerializeField] [Required] private Texture2D _cursorTexture;
+		private CursorMode _cursorMode = CursorMode.Auto;
+		private Vector2 _hotSpot = Vector2.zero;
+		private void Awake()
+		{
+			Cursor.SetCursor(_cursorTexture, _hotSpot, _cursorMode);
+		}
+	}
+
+}
